010cee33cf5f1dc3e4bf7b83f45d47ed8b41090c
LabeedIKhan <69700126+LabeedIKhan@users.noreply.github.com>
2020-10-15 21:44:52 +0100

    Add files via upload

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,36 @@
+"""tango_with_django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+# mapping URL for user
+# so user can see view
+# importing views
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+# following code creates intial mapping
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # this new code looks for url string that matches ^rango/
+    url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# server does shows new message 
+
